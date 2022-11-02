799844a2e0500a076111446b596e839da835f086
Pieter-Deconinck <pieter.deconinck@student.hogent.be>
2022-11-02 23:11:17 +0100

    Labo 4 linux not completed yet

---
+++ Scripts/Labo 4 script 1.sh
@@ -0,0 +1,8 @@
+  GNU nano 5.6.1                                                              params.sh                                                                         
+#!/bin/bash
+
+echo "Script name: ${0}"
+echo "num params: ${#}"
+echo "Param 1: ${1}"
+echo "Param 3: ${3}"
+echo "Param 10: ${10}"
